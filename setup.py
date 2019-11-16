2a017ad92255ffcf52d76b19f930b7f95ff87d9c
Skile <imleejm@gmail.com>
2019-11-16 16:41:54 +0900

    Make package with setuptools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simplethoth',
-    version='0.1.0',
+    version='0.1.4',
     author='Jeongmin Lee (Skile)',
     author_email='imleejm@gmail.com',
     packages=find_packages(),
